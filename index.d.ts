accf0f78530212d8a2cd9d43d01457392a36d13d
Wesley Sousa <wesleytuck@outlook.com>
2024-08-27 11:43:50 -0300

    ajustes

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export * from './src/enums/index';
+export * from './src/interfaces/unidade-negocio/index';
+export * from './src/interfaces/usuario/index';
+export * from './src/interfaces/vendedor/index';
