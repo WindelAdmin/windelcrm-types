f104791ed2c9b8219030e83d92d16d3ac407320b
Wesley Sousa <wesleytuck@outlook.com>
2024-08-28 08:49:21 -0300

    ajustes

--- @core/index.ts
+++ @core/index.ts
@@ -2,6 +2,6 @@ export * from './enums/DiaSemana';
 export * from './enums/TipoUnidadeNegocio';
 export * from './enums/UsuarioSetor';
 
-export * from './@types/unidade-negocio/UnidadeNegocio';
-export * from './@types/usuario/PermissaoModel';
-export * from './@types/usuario/Usuario';
+export * from './types/unidade-negocio/UnidadeNegocio';
+export * from './types/usuario/PermissaoModel';
+export * from './types/usuario/Usuario';
